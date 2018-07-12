b63720cb54bb06af07490aefb8af642d9d50027f
botnaysard <botnaysard@protonmail.com>
2018-07-12 14:20:15 -0400

    first commit

---
+++ insight.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	$('.disc').click(function(){
+		var which = $(this).attr("id");
+
+	});
+
+	/* USE THIS G2G CODE AS A STARTING POINT.  WILL HAVE TO HIDE this.parent AND show.parent for the selected item.
+
+	$('.staff-box').click(function(){
+		var whoisit = $(this).attr('id');
+		$('.staff-box').not('#' + whoisit).fadeOut(600);
+		$('#' + whoisit +'-bio').slideDown(600, "swing");
+	});
+
+	*/
+		
+	
+
+});
