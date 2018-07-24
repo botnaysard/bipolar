6e714c843be8af614d539eb62665562e064fe50f
botnaysard <botnaysard@protonmail.com>
2018-07-24 15:25:44 -0400

    starting with charts need to get json data into a 2d array for chartist

--- js/results.js
+++ js/results.js
@@ -9,13 +9,20 @@ $(document).ready(function(){
 
     $.getJSON("https://api.mlab.com/api/1/databases/moodtracker/collections/mtracked?apiKey=ybAPUS6CcJVkdlwxn0LxCHtbbZVUgtQg", function (data) {
         for (var i = 0; i < data.length; i++) {
-            moodTimeline.push(Number([data[i].moodRating]));
+            moodTimeline.push(data[i].moodRating);
             nodeNumber.push(i);
         }    
     });
 
     console.log(moodTimeline);
     console.log(nodeNumber);
+    var lineData = {
+      labels: nodeNumber,
+      series: moodTimeline
+    };
+    var options = {
+    };
+    new Chartist.Line('.ct-chart', lineData, options);
     
 
 });
